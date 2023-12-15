722c0f98fcc2051b7a789e1e2d71b88013dea1cb
RubenRoman <ruben.roman@forcast.cl>
2023-12-15 11:09:32 -0300

    :bug: fix backend to mount front

--- back/index.js
+++ back/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = require('./src/routes/routes');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 //Middlewares
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json({ limit: '10mb' }))
